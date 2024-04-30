bedfa5e56187b0a2c6c6b5c20ec9187ca752f3c7
Pedro Vieira <pedro.silva@pagtel.com.br>
2024-04-30 06:58:51 -0300

    feat: add search bar and on tap on view alld

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:fluentui_icons/fluentui_icons.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_study_app/utils/app_styles.dart';
 import 'package:gap/gap.dart';
@@ -12,7 +13,7 @@ class HomePage extends StatelessWidget {
         body: ListView(
           children: [
             Container(
-                padding: const EdgeInsets.symmetric(horizontal: 20),
+                padding: const EdgeInsets.symmetric(horizontal: 25),
                 child: Column(children: [
                   const Gap(40),
                   Row(
@@ -32,8 +33,43 @@ class HomePage extends StatelessWidget {
                             borderRadius: BorderRadius.circular(12),
                             image: const DecorationImage(
                                 fit: BoxFit.fitHeight,
-                                image: AssetImage("assets/images/logo.jpg")
-                            )
+                                image: AssetImage("assets/images/logo.jpg"))),
+                      )
+                    ],
+                  ),
+                  const Gap(20),
+                  Container(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 20, vertical: 10),
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(12),
+                        color: const Color(0xFFF4F6FD)),
+                    child: Row(
+                      children: [
+                        const Icon(FluentSystemIcons.ic_fluent_search_regular,
+                            color: Color(0xFFBFC205)),
+                        const Gap(5),
+                        Text(
+                          "Search",
+                          style: Styles.headLineStyle4,
+                        )
+                      ],
+                    ),
+                  ),
+                  const Gap(40),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "Upcoming flights",
+                        style: Styles.headLineStyle2,
+                      ),
+                      InkWell(
+                        onTap: () {},
+                        child: Text(
+                          "View all",
+                          style: Styles.textStyle
+                              .copyWith(color: Styles.primaryColor),
                         ),
                       )
                     ],
